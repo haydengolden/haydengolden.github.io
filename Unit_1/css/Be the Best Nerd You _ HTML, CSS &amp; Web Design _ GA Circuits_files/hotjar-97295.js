1b7d328bb193f79674e1754494183f6af792a7df
haydengolden <hayden.golden@gmail.com>
2016-06-04 13:45:31 -0400

    Testing New Ideas

---
+++ Unit_1/css/Be the Best Nerd You _ HTML, CSS &amp; Web Design _ GA Circuits_files/hotjar-97295.js
@@ -0,0 +1,9 @@
+window.hjSiteSettings = {"testers_widgets":[],"recording_capture_keystrokes":true,"site_id":97295,"deferred_page_contents":[],"record_targeting_rules":[],"surveys":[],"heatmaps":[],"polls":[],"forms":[],"record":false,"r":1.0};
+
+window.hjBootstrap = function (scriptUrl) {
+    var s = document.createElement('script');
+    s.src = scriptUrl;
+    document.getElementsByTagName('head')[0].appendChild(s);
+};
+
+hjBootstrap('https://script.hotjar.com/modules-1c274a5f10395a0cf99b8fe28234396f.js');
